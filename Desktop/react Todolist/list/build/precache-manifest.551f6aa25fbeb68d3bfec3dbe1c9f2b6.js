fce3d23feca1146ffe54e2e07666e40ad274e51d
xiaoshengkang <898987943@qq.com>
2019-04-02 12:45:05 +0800

    build x 3

---
+++ Desktop/react Todolist/list/build/precache-manifest.551f6aa25fbeb68d3bfec3dbe1c9f2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//sean103.github.io/ToDoList/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d8a40d4ee024edc06326",
+    "url": "//sean103.github.io/ToDoList/build/static/js/runtime~main.b06e1438.js"
+  },
+  {
+    "revision": "64d12ee18e35ee472f1c",
+    "url": "//sean103.github.io/ToDoList/build/static/js/main.cce8cb2a.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "//sean103.github.io/ToDoList/build/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "64d12ee18e35ee472f1c",
+    "url": "//sean103.github.io/ToDoList/build/static/css/main.017c0216.chunk.css"
+  },
+  {
+    "revision": "4ce891da0d3377d0b6e4eb96cf95156e",
+    "url": "//sean103.github.io/ToDoList/build/index.html"
+  }
+];
